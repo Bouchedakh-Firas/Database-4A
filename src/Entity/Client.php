4b075b99d80c7822bde675f33715d3117c573f52
Firas_Bch <79410428+Bouchedakh-Firas@users.noreply.github.com>
2023-01-23 20:24:33 +0100

    adde client commande relation

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -51,9 +51,15 @@ class Client
      */
     private $numero;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Commande::class, mappedBy="client")
+     */
+    private $commandes;
+
     public function __construct()
     {
         $this->addresses = new ArrayCollection();
+        $this->commandes = new ArrayCollection();
     }
 
     public function getIdclient(): ?int
@@ -133,6 +139,36 @@ class Client
         }
 
         $this->numero = $numero;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Commande>
+     */
+    public function getCommandes(): Collection
+    {
+        return $this->commandes;
+    }
+
+    public function addCommande(Commande $commande): self
+    {
+        if (!$this->commandes->contains($commande)) {
+            $this->commandes[] = $commande;
+            $commande->setClient($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCommande(Commande $commande): self
+    {
+        if ($this->commandes->removeElement($commande)) {
+            // set the owning side to null (unless already changed)
+            if ($commande->getClient() === $this) {
+                $commande->setClient(null);
+            }
+        }
 
         return $this;
     }
